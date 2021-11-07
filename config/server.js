11beec8a8ab3eb9fc457e72c0b672033eec2b659
Alhasan-softwear <alhasan.feyikemi@gmail.com>
2021-11-07 18:18:35 +0100

    new

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1b4323510e148e07d3ea1eee9d9701ca'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd7c6a403-3567-43ca-ba8c-fefe4ba84c2f'
+};
